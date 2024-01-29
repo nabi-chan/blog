d0ab7f062605cf7150f8cff71c79a463346d3d9e
nabi.lee <hello@nabi.kim>
2024-01-30 05:58:33 +0900

    Update Basic Layout

--- src/themes/layouts/Basic.tsx
+++ src/themes/layouts/Basic.tsx
@@ -8,15 +8,20 @@ export const BasicLayout = ({ children }: { children: ReactNode }) => {
   const title = `${config.titlePrefix}${opts.title}`;
 
   return (
-    <article className="nx-container nx-prose max-md:nx-prose-sm dark:nx-prose-dark">
+    <main className="nx-container nx-prose max-md:nx-prose-sm dark:nx-prose-dark">
       <Head>
         <title>{title}</title>
         {config.head?.({ title, meta: opts.frontMatter })}
       </Head>
 
-      <h1>{opts.title}</h1>
-      {children}
-      {config.footer}
-    </article>
+      <header className="prose-h1:nx-mb-0 prose-p:nx-mt-2 prose-p:text-xs">
+        <h1>{opts.title}</h1>
+        <p>{opts.frontMatter?.description}</p>
+      </header>
+
+      <article>{children}</article>
+
+      <footer>{config.footer}</footer>
+    </main>
   );
 };
--- theme.config.tsx
+++ theme.config.tsx
@@ -3,8 +3,10 @@ import { BlogPageConfig } from "@/themes/types/types";
 export default {
   titlePrefix: "🐈 > ",
   footer: (
-    <footer className="nx-flex nx-items-center nx-justify-between nx-not-prose nx-text-xs nx-pt-12">
-      <span>@nabi-chan</span>
+    <footer className="nx-flex nx-items-center nx-justify-between nx-text-xs nx-pt-20">
+      <span>
+        Authored by <a href="https://github.com/nabi-chan">@nabi-chan</a>
+      </span>
       <ul className="nx-flex nx-gap-2 nx-list-none nx-font-bold">
         <li>
           <a href="https://github.com/nabi-chan">Github</a>
