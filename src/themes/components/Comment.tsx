18c0ca3ae22265bbd1fe8193f6d55fb5b308c6c4
nabi.lee <hello@nabi.kim>
2024-02-07 12:49:42 +0900

    fix: fix comment not changed by theme

--- src/themes/components/Comment.tsx
+++ src/themes/components/Comment.tsx
@@ -1,4 +1,8 @@
+import { useTheme } from 'next-themes';
+
 export function Comment() {
+  const { theme } = useTheme();
+
   return (
     <aside
       ref={(element) => {
@@ -11,11 +15,13 @@ export function Comment() {
         script.src = 'https://utteranc.es/client.js';
         script.setAttribute('repo', 'nabi-chan/comments');
         script.setAttribute('issue-term', 'title');
-        script.setAttribute('theme', 'preferred-color-scheme');
+        script.setAttribute('theme', theme === 'dark' ? 'github-dark' : 'github-light');
         script.setAttribute('crossorigin', 'anonymous');
 
         if (element.children.length === 0) {
           element.appendChild(script);
+        } else {
+          element.replaceChild(script, element.children[0]);
         }
       }}
     />
