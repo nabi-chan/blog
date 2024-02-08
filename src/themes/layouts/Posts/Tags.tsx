a6fce55a2a8c497b5e2fda47998585715e61751b
nabi.lee <hello@nabi.kim>
2024-02-08 21:07:49 +0900

    feat: add tags panel

--- src/themes/layouts/Posts/Tags.tsx
+++ src/themes/layouts/Posts/Tags.tsx
@@ -1,40 +1,26 @@
 import { Badge } from '@/themes/components/Badge';
+import { Link } from '@/themes/components/Link';
+import { useBlogContext } from '@/themes/contexts/blogContext';
+import { getUniqueTags } from '@/themes/utils/tags';
 
 export function Tags() {
+  const { opts } = useBlogContext();
+  const tags = getUniqueTags(opts.pageMap);
+
   return (
     <aside className="flex flex-col gap-2">
       <h1 className="text-xl font-bold">태그 목록</h1>
       <ul className="flex flex-wrap gap-2">
-        <li>
-          <Badge className="text-sm">#Tag1</Badge>
-        </li>
-        <li>
-          <Badge className="text-sm">#Tag2</Badge>
-        </li>
-        <li>
-          <Badge className="text-sm">#Tag3</Badge>
-        </li>
-        <li>
-          <Badge className="text-sm">#Tag4</Badge>
-        </li>
-        <li>
-          <Badge className="text-sm">#Tag5</Badge>
-        </li>
-        <li>
-          <Badge className="text-sm">#Tag6</Badge>
-        </li>
-        <li>
-          <Badge className="text-sm">#Tag7</Badge>
-        </li>
-        <li>
-          <Badge className="text-sm">#Tag8</Badge>
-        </li>
-        <li>
-          <Badge className="text-sm">#Tag9</Badge>
-        </li>
-        <li>
-          <Badge className="text-sm">#Tag10</Badge>
-        </li>
+        {tags.slice(0, 10).map((tag) => (
+          <li key={tag}>
+            <Link href={`/tag/${tag}`}>
+              <Badge className="text-sm">#{tag}</Badge>
+            </Link>
+          </li>
+        ))}
+        <Link href="/tags" className="text-sm text-zinc-500">
+          더보기
+        </Link>
       </ul>
     </aside>
   );
