de7910ec127985caa7d06a89ca94c8853d5a00f9
nabi.lee <hello@nabi.kim>
2024-02-10 00:50:52 +0900

    fix: fix build

--- src/themes/components/nextra/index.ts
+++ src/themes/components/nextra/index.ts
@@ -1,6 +1,5 @@
 export { Code } from './code';
 export { Pre } from './pre';
-export { Tabs, Tab } from './tabs';
 export { Td } from './td';
 export { Table } from './table';
 export { Th } from './th';
