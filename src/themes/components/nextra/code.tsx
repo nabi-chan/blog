794187aef6b53ea6528e22468ffee1485fed280c
nabi.lee <hello@nabi.kim>
2024-02-13 09:22:54 +0900

    fix: fix darkmode style not setted

--- src/themes/components/nextra/code.tsx
+++ src/themes/components/nextra/code.tsx
@@ -6,7 +6,7 @@ export const Code = ({ children, className, ...props }: ComponentProps<'code'>):
   return (
     <code
       className={cn(
-        'border-black border-opacity-[0.04] bg-opacity-[0.03] bg-black break-words rounded-md border py-0.5 px-[.25em] text-[.9em] text-blue-950',
+        'border-black border-opacity-[0.04] bg-opacity-[0.03] bg-black break-words rounded-md border py-0.5 px-[.25em] text-[.9em] text-blue-500',
         'dark:border-white/10 dark:bg-white/10',
         hasLineNumbers && '[counter-reset:line]',
         className,
