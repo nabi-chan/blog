fca641ef6c02cd33c2ff641b658f31e259ff2aa9
nabi <42037851+nabi-chan@users.noreply.github.com>
2024-02-13 21:38:12 +0900

    chore: add eslint for linting (#7)

---
+++ .changeset/honest-bulldogs-wink.md
@@ -0,0 +1,6 @@
+---
+"nabi-blog": patch
+---
+
+- b09c446e refactor: apply prettier
+- 894a96a9 chore: add eslint
--- src/themes/components/nextra/icons/copy.tsx
+++ src/themes/components/nextra/icons/copy.tsx
@@ -1,4 +1,4 @@
-import type { ComponentProps, ReactElement } from 'react'
+import type { ComponentProps, ReactElement } from 'react';
 
 export function CopyIcon(props: ComponentProps<'svg'>): ReactElement {
   return (
@@ -11,16 +11,7 @@ export function CopyIcon(props: ComponentProps<'svg'>): ReactElement {
       stroke="currentColor"
       {...props}
     >
-      <rect
-        x="9"
-        y="9"
-        width="13"
-        height="13"
-        rx="2"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <rect x="9" y="9" width="13" height="13" rx="2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
       <path
         d="M5 15H4C2.89543 15 2 14.1046 2 13V4C2 2.89543 2.89543 2 4 2H13C14.1046 2 15 2.89543 15 4V5"
         strokeWidth="2"
@@ -28,5 +19,5 @@ export function CopyIcon(props: ComponentProps<'svg'>): ReactElement {
         strokeLinejoin="round"
       />
     </svg>
-  )
+  );
 }
