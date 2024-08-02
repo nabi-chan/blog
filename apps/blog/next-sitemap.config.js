84ca944ef6af7c96c9791cbf397a09076712319a
nabi.lee <hello@nabi.kim>
2024-08-02 17:28:26 +0900

    feat: add ghost-sitemap.xml

--- apps/blog/next-sitemap.config.js
+++ apps/blog/next-sitemap.config.js
@@ -2,4 +2,8 @@
 export default {
   siteUrl: 'https://nabi-blog.vercel.app',
   generateRobotsTxt: true,
+  exclude: ['/ghost-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://nabi-blog.vercel.app/ghost-sitemap.xml'],
+  },
 }
---
+++ apps/blog/src/features/sitemap/getServerSideProps.ts
@@ -0,0 +1,20 @@
+import { content } from 'Libs/ghost/content'
+import type { GetServerSideProps } from 'next'
+import type { ISitemapField } from 'next-sitemap'
+import { getServerSideSitemapLegacy } from 'next-sitemap'
+import config from 'next-sitemap.config'
+
+export const getServerSideProps = (async (context) => {
+  const posts = await content.posts.browse()
+  const pages = await content.pages.browse()
+
+  const paths = [...posts, ...pages]
+
+  const fields: ISitemapField[] = paths.map((page) => ({
+    loc: `${config.siteUrl}${page.slug.replace('index', '')}`,
+    lastmod: new Date(page.updated_at ?? +new Date()).toISOString(),
+    changefreq: 'always',
+  }))
+
+  return getServerSideSitemapLegacy(context, fields)
+}) satisfies GetServerSideProps
