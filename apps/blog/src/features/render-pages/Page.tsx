0e64083d7fbf27a31c2b3bc79c9be9781c3a5d69
nabi.lee <hello@nabi.kim>
2024-08-02 18:21:34 +0900

    feat: add get page info from ghost

--- apps/blog/src/features/render-pages/Page.tsx
+++ apps/blog/src/features/render-pages/Page.tsx
@@ -1,5 +1,6 @@
+import { Text } from '@channel.io/bezier-react'
 import type { PageProps } from './getStaticProps'
 
-export function Page({}: PageProps) {
-  return <></>
+export function Page(props: PageProps) {
+  return <Text as="pre">{JSON.stringify(props, null, 2)}</Text>
 }
