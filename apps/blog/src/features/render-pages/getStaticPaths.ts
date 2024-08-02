4793295f5cffbffd292b7dfbcd1dd8ac1467ec58
nabi.lee <hello@nabi.kim>
2024-08-02 16:16:27 +0900

    feat: add getStaticPaths from post / blog routes

--- apps/blog/src/features/render-pages/getStaticPaths.ts
+++ apps/blog/src/features/render-pages/getStaticPaths.ts
@@ -1,8 +1,26 @@
+import type { PostOrPage } from '@tryghost/content-api'
+import { content } from 'Libs/ghost/content'
+import { isEmpty } from 'lodash-es'
 import type { GetStaticPaths } from 'next'
 
+const toSlugArray = (entity: PostOrPage) => {
+  const list = entity.slug
+    .replace('index', '')
+    .split('/')
+    .filter((v) => !isEmpty(v))
+  return !isEmpty(list) ? list : undefined
+}
+
+const toPathParams = (slug?: string[]) => ({ params: { slug } })
+
 export const getStaticPaths = (async () => {
+  const posts = await content.posts.browse()
+  const pages = await content.pages.browse()
+
+  const paths = [...posts.map(toSlugArray), ...pages.map(toSlugArray)]
+
   return {
-    paths: [],
+    paths: paths.map(toPathParams),
     fallback: false,
   }
 }) satisfies GetStaticPaths
