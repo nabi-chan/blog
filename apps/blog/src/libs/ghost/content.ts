51a2874b31f63dc9d9304e828343596f3b9238ba
nabi.lee <hello@nabi.kim>
2024-08-03 13:26:40 +0900

    refactor: use @ts-ghost/admin-api instead of official api

--- apps/blog/src/libs/ghost/content.ts
+++ apps/blog/src/libs/ghost/content.ts
@@ -3,5 +3,5 @@ import GhostContentAPI from '@tryghost/content-api'
 export const content = new GhostContentAPI({
   url: 'http://127.0.0.1:4000',
   key: process.env.GHOST_CONTENT_KEY!,
-  version: 'v5.88',
+  version: 'v5.88.2',
 })
