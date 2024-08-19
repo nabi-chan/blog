e88a0dd45b27a178ae78e749b067d46ef9612c91
nabi.lee <hello@nabi.kim>
2024-08-19 22:22:20 +0900

    chore: use another supabase instance

--- apps/blog/src/libs/supabase/types.ts
+++ apps/blog/src/libs/supabase/types.ts
@@ -9,75 +9,7 @@ export type Json =
 export type Database = {
   public: {
     Tables: {
-      Article: {
-        Row: {
-          category: Database['public']['Enums']['BlogCategory'] | null
-          content: string
-          created_at: string
-          description: string | null
-          id: string
-          stage: Database['public']['Enums']['Stage'] | null
-          title: string
-          updated_at: string
-        }
-        Insert: {
-          category?: Database['public']['Enums']['BlogCategory'] | null
-          content: string
-          created_at?: string
-          description?: string | null
-          id?: string
-          stage?: Database['public']['Enums']['Stage'] | null
-          title: string
-          updated_at?: string
-        }
-        Update: {
-          category?: Database['public']['Enums']['BlogCategory'] | null
-          content?: string
-          created_at?: string
-          description?: string | null
-          id?: string
-          stage?: Database['public']['Enums']['Stage'] | null
-          title?: string
-          updated_at?: string
-        }
-        Relationships: []
-      }
-      CustomPage: {
-        Row: {
-          content: string | null
-          created_at: string
-          description: string | null
-          id: number
-          layout: Database['public']['Enums']['PageLayout']
-          slug: string
-          title: string
-          type: Database['public']['Enums']['PageType']
-          updated_at: string
-        }
-        Insert: {
-          content?: string | null
-          created_at?: string
-          description?: string | null
-          id?: number
-          layout?: Database['public']['Enums']['PageLayout']
-          slug: string
-          title: string
-          type?: Database['public']['Enums']['PageType']
-          updated_at?: string
-        }
-        Update: {
-          content?: string | null
-          created_at?: string
-          description?: string | null
-          id?: number
-          layout?: Database['public']['Enums']['PageLayout']
-          slug?: string
-          title?: string
-          type?: Database['public']['Enums']['PageType']
-          updated_at?: string
-        }
-        Relationships: []
-      }
+      [_ in never]: never
     }
     Views: {
       [_ in never]: never
@@ -86,10 +18,7 @@ export type Database = {
       [_ in never]: never
     }
     Enums: {
-      BlogCategory: 'TECHNICAL' | 'JOURNAL' | 'NOTE'
-      PageLayout: 'SITE' | 'WIDE' | 'BASE' | 'EMPTY'
-      PageType: 'MARKDOWN' | 'HTML'
-      Stage: 'PRIVATE' | 'PUBLIC' | 'LINK_ONLY' | 'DRAFT'
+      [_ in never]: never
     }
     CompositeTypes: {
       [_ in never]: never
@@ -97,27 +26,27 @@ export type Database = {
   }
 }
 
-type PublicSchema = Database[Extract<keyof Database, 'public'>]
+type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
   PublicTableNameOrOptions extends
-    | keyof (PublicSchema['Tables'] & PublicSchema['Views'])
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
-        Database[PublicTableNameOrOptions['schema']]['Views'])
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
-      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] &
-        PublicSchema['Views'])
-    ? (PublicSchema['Tables'] &
-        PublicSchema['Views'])[PublicTableNameOrOptions] extends {
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
         Row: infer R
       }
       ? R
@@ -126,19 +55,19 @@ export type Tables<
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
-    | keyof PublicSchema['Tables']
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Insert: infer I
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
-    ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
         Insert: infer I
       }
       ? I
@@ -147,19 +76,19 @@ export type TablesInsert<
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
-    | keyof PublicSchema['Tables']
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Update: infer U
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
-    ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
         Update: infer U
       }
       ? U
@@ -168,13 +97,13 @@ export type TablesUpdate<
 
 export type Enums<
   PublicEnumNameOrOptions extends
-    | keyof PublicSchema['Enums']
+    | keyof PublicSchema["Enums"]
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
-    ? PublicSchema['Enums'][PublicEnumNameOrOptions]
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
     : never
