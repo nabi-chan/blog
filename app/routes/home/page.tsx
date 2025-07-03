59318c3e1d26f1d75242a4a6ba383d20724a9cd8
nabi.lee <hello@nabi.kim>
2025-07-03 21:35:49 +0900

    feat: 홈 페이지 추가

--- app/routes/home/page.tsx
+++ app/routes/home/page.tsx
@@ -1,3 +1,39 @@
+import { Anchor, Center, List, Stack, Text } from "@mantine/core"
+
 export default function Page() {
-  return <></>
+  return (
+    <Center h="calc(100dvh - 60px - var(--app-shell-padding) * 2)">
+      <Stack>
+        <Text fz="h1" ta="center" ff="Tossface">
+          🦋
+        </Text>
+
+        <Text fz="h4" ta="center" fw={600}>
+          Software Engineer in{" "}
+          <Anchor fw="bold" href="https://github.com/cat-hou-se">
+            cat-house
+          </Anchor>
+        </Text>
+
+        <List>
+          <List.Item>
+            <Anchor fw={600} href="mailto:hello@nabi.kim">
+              <Text component="span" fz="xl" ff="Tossface" mr={4}>
+                ✉️
+              </Text>
+              Email
+            </Anchor>
+          </List.Item>
+          <List.Item>
+            <Anchor fw={600} href="https://nabi.kim/s/cv">
+              <Text component="span" fz="xl" ff="Tossface" mr={4}>
+                📄
+              </Text>
+              CV
+            </Anchor>
+          </List.Item>
+        </List>
+      </Stack>
+    </Center>
+  )
 }
